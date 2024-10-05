9aad3be2d8ed77c3116ad4c6c4446cc1df1a3d23
simone.caldon <simone.caldon@trovaprezzi.it>
2024-10-05 15:04:19 +0200

    refactor

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,4 @@
 import { Scene } from "phaser";
-import { DISPLAY_HEIGHT, DISPLAY_WIDTH } from "../main";
 
 export class Game extends Scene {
   constructor() {
@@ -7,70 +6,15 @@ export class Game extends Scene {
   }
 
   create() {
-    // create map
-    const map = this.make.tilemap({ key: "tilemap" });
-    const worldWidth = map.tileWidth * map.width;
-    const worldHeight = map.tileHeight * map.height;
-    const tileset = map.addTilesetImage("super-mario", "super_mario_tiles");
-    // create background layer
-    map.createLayer("Background", tileset);
-    // create platform layer and set collision on its tiles
-    const platformTilesLayer = map.createLayer("Platforms", tileset);
-    platformTilesLayer.setCollisionByExclusion([-1]);
+    const { worldWidth, worldHeight, platformTilesLayer } =
+      this._createTileMap();
 
-    // create player and set collision on word bounds
-    this.player = this.physics.add.sprite(32, 0, "mario");
-    this.playerDirection = "right";
-    this.player.setCollideWorldBounds(true);
+    this._createPlayer();
+    this._createAnimations();
 
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("mario", { start: 3, end: 1 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("mario", { start: 6, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: "turn-right",
-      frames: [{ key: "mario", frame: 5 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "turn-left",
-      frames: [{ key: "mario", frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "jump-right",
-      frames: [{ key: "mario", frame: 9 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "jump-left",
-      frames: [{ key: "mario", frame: 0 }],
-      frameRate: 20,
-    });
-
-    // add player and platform layer collision
     this.physics.add.collider(this.player, platformTilesLayer);
-
-    // grap keyboard input
     this.cursors = this.input.keyboard.createCursorKeys();
-
-    // set camera
-    this.physics.world.setBounds(0, 0, worldWidth, worldHeight);
-    this.cameras.main.setBounds(0, 0, worldWidth, worldHeight);
-    this.cameras.main.startFollow(this.player);
+    this._configureCamera(worldWidth, worldHeight, this.player);
   }
 
   update() {
@@ -97,5 +41,67 @@ export class Game extends Scene {
       this.player.setVelocityY(-230);
       this.player.anims.play(`jump-${this.playerDirection}`);
     }
+  }
+
+  _createTileMap() {
+    // create map
+    const map = this.make.tilemap({ key: "tilemap" });
+    const worldWidth = map.tileWidth * map.width;
+    const worldHeight = map.tileHeight * map.height;
+    const tileset = map.addTilesetImage("super-mario", "super_mario_tiles");
+    // create background layer
+    map.createLayer("Background", tileset);
+    // create platform layer and set collision on its tiles
+    const platformTilesLayer = map.createLayer("Platforms", tileset);
+    platformTilesLayer.setCollisionByExclusion([-1]);
+
+    return { worldWidth, worldHeight, platformTilesLayer };
+  }
+
+  _createPlayer() {
+    this.player = this.physics.add.sprite(32, 0, "mario");
+    this.playerDirection = "right";
+    this.player.setCollideWorldBounds(true);
+  }
+
+  _createAnimations() {
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("mario", { start: 3, end: 1 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("mario", { start: 6, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "turn-right",
+      frames: [{ key: "mario", frame: 5 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: "turn-left",
+      frames: [{ key: "mario", frame: 4 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: "jump-right",
+      frames: [{ key: "mario", frame: 9 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: "jump-left",
+      frames: [{ key: "mario", frame: 0 }],
+      frameRate: 20,
+    });
+  }
+
+  _configureCamera(width, height, player) {
+    this.physics.world.setBounds(0, 0, width, height);
+    this.cameras.main.setBounds(0, 0, width, height);
+    this.cameras.main.startFollow(player);
   }
 }
