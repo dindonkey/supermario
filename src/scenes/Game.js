76f42884dcb35f865110b25202d8aac03887fbc8
simone.caldon <simone.caldon@trovaprezzi.it>
2024-10-05 14:20:46 +0200

    fix mario anim

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -22,8 +22,35 @@ export class Game extends Scene
         platformTilesLayer.setCollisionByExclusion([-1])
 
         // create player and set collision on word bounds
-        this.player = this.physics.add.image(16, 0, 'mario');
+        this.player =  this.physics.add.sprite(15, 0, 'mario');
+        this.playerDirection = 'right'
         this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('mario', { start: 3, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('mario', { start: 6, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn-right',
+            frames: [{ key: 'mario', frame: 5 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'turn-left',
+            frames: [{ key: 'mario', frame: 4 }],
+            frameRate: 20
+        });
 
         // add player and platform layer collision
         this.physics.add.collider(this.player, platformTilesLayer);
@@ -40,12 +67,17 @@ export class Game extends Scene
     update () {
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
+            this.playerDirection = 'left';
+            this.player.anims.play('left', true);
         }
         else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);
+            this.playerDirection = 'right';
+            this.player.anims.play('right', true);
         }
         else {
             this.player.setVelocityX(0);
+            this.player.anims.play(`turn-${this.playerDirection}`);
         }
     
         if (this.cursors.up.isDown ) {
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -10,8 +10,11 @@ export class Preloader extends Scene
     preload ()
     {
         this.load.image('super_mario_tiles', 'assets/super-mario.png')
-        this.load.image('mario', 'assets/mario.png')
         this.load.tilemapTiledJSON('tilemap', 'assets/super_mario_level.json')
+        this.load.spritesheet('mario',
+            'assets/mario_sprite.png',
+            { frameWidth: 16, frameHeight: 16 }
+        );
     }
 
     create ()
