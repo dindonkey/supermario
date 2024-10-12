b0ab060ff8c2318c68b5f0c24d42d8a48894512d
simone.caldon <simone.caldon@trovaprezzi.it>
2024-10-12 22:21:44 +0200

    refactor: extract player object

---
+++ src/models/player.js
@@ -0,0 +1,88 @@
+import { Physics } from "phaser";
+
+const SPRITE_KEY = 'mario';
+const LEFT = 'left';
+const RIGHT = 'right';
+const WALK_LEFT_ANIM_KEY = 'walk-left';
+const WALK_RIGHT_ANIM_KEY = 'walk-right';
+const TURN_ANIM_KEY = 'turn';
+const JUMP_ANIM_KEY = 'jump';
+const VELOCITY_X = 160;
+const VELOCITY_Y = 230;
+
+class Player extends Physics.Arcade.Sprite {
+
+  constructor({ scene, x, y}) {
+    super(scene, x ,y, SPRITE_KEY);
+    scene.add.existing(this);
+    scene.physics.add.existing(this, false);
+    this.playerDirection = RIGHT;
+    this.setCollideWorldBounds(true);
+    this._createAnimations();
+    this.cursors = scene.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.setVelocityX(VELOCITY_X*-1);
+      this.playerDirection = LEFT;
+
+      if (this.body.blocked.down) {
+        this.anims.play(WALK_LEFT_ANIM_KEY, true);
+      }
+    } else if (this.cursors.right.isDown) {
+      this.setVelocityX(VELOCITY_X);
+      this.playerDirection = RIGHT;
+      if (this.body.blocked.down) {
+        this.anims.play(WALK_RIGHT_ANIM_KEY, true);
+      }
+    } else {
+      this.setVelocityX(0);
+      if (this.body.blocked.down) {
+        this.anims.play(`${TURN_ANIM_KEY}-${this.playerDirection}`);
+      }
+    }
+
+    if (this.cursors.up.isDown && this.body.blocked.down) {
+      this.setVelocityY(VELOCITY_Y*-1);
+      this.anims.play(`${JUMP_ANIM_KEY}-${this.playerDirection}`);
+    }
+  }
+
+  _createAnimations() {
+    this.anims.create({
+      key: WALK_LEFT_ANIM_KEY,
+      frames: this.anims.generateFrameNumbers(SPRITE_KEY, { start: 3, end: 1 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: WALK_RIGHT_ANIM_KEY,
+      frames: this.anims.generateFrameNumbers(SPRITE_KEY, { start: 6, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: `${TURN_ANIM_KEY}-${RIGHT}`,
+      frames: [{ key: SPRITE_KEY, frame: 5 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: `${TURN_ANIM_KEY}-${LEFT}`,
+      frames: [{ key: SPRITE_KEY, frame: 4 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: `${JUMP_ANIM_KEY}-${RIGHT}`,
+      frames: [{ key: SPRITE_KEY, frame: 9 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: `${JUMP_ANIM_KEY}-${LEFT}`,
+      frames: [{ key: SPRITE_KEY, frame: 0 }],
+      frameRate: 20,
+    });
+  }
+}
+
+export default Player;
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,4 +1,5 @@
 import { Scene } from "phaser";
+import Player from "../models/player";
 
 export class Game extends Scene {
   constructor() {
@@ -9,9 +10,8 @@ export class Game extends Scene {
     const { worldWidth, worldHeight, platformTilesLayer } =
       this._createTileMap();
 
-    this._createPlayer();
-    this._createAnimations();
-
+    this.player = new Player({ scene: this, x: 32, y: 0 });
+    
     this.physics.add.collider(this.player, platformTilesLayer);
     this.physics.add.collider(
       this.player,
@@ -21,34 +21,11 @@ export class Game extends Scene {
       this
     );
 
-    this.cursors = this.input.keyboard.createCursorKeys();
     this._configureCamera(worldWidth, worldHeight, this.player);
   }
 
   update() {
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.playerDirection = "left";
-      if (this.player.body.blocked.down) {
-        this.player.anims.play("left", true);
-      }
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-      this.playerDirection = "right";
-      if (this.player.body.blocked.down) {
-        this.player.anims.play("right", true);
-      }
-    } else {
-      this.player.setVelocityX(0);
-      if (this.player.body.blocked.down) {
-        this.player.anims.play(`turn-${this.playerDirection}`);
-      }
-    }
-
-    if (this.cursors.up.isDown && this.player.body.blocked.down) {
-      this.player.setVelocityY(-230);
-      this.player.anims.play(`jump-${this.playerDirection}`);
-    }
+    this.player.update();
   }
 
   _createTileMap() {
@@ -71,47 +48,6 @@ export class Game extends Scene {
     });
 
     return { worldWidth, worldHeight, platformTilesLayer };
-  }
-
-  _createPlayer() {
-    this.player = this.physics.add.sprite(32, 0, "mario");
-    this.playerDirection = "right";
-    this.player.setCollideWorldBounds(true);
-  }
-
-  _createAnimations() {
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("mario", { start: 3, end: 1 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("mario", { start: 6, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: "turn-right",
-      frames: [{ key: "mario", frame: 5 }],
-      frameRate: 20,
-    });
-    this.anims.create({
-      key: "turn-left",
-      frames: [{ key: "mario", frame: 4 }],
-      frameRate: 20,
-    });
-    this.anims.create({
-      key: "jump-right",
-      frames: [{ key: "mario", frame: 9 }],
-      frameRate: 20,
-    });
-    this.anims.create({
-      key: "jump-left",
-      frames: [{ key: "mario", frame: 0 }],
-      frameRate: 20,
-    });
   }
 
   _configureCamera(width, height, player) {
